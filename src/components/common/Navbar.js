3e2f790bad0a64b596ebec38e930e61da323c103
eneamtiu <42860002+eneamtiu@users.noreply.github.com>
2021-02-04 00:40:05 +0200

    Remove unnecesary pseudo element

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -4,28 +4,26 @@ import { GiBookmark } from "react-icons/gi";
 
 function Navbar() {
   return (
-    <>
-      <div class="container d-flex align-items-center">
-        <h1 class="logo me-auto">
-          <NavLink to="/" exact className="link">
-            <GiBookmark className="logo" /> <span id="logo-name">e</span>
-            <span id="logo-text">Learning</span>
-          </NavLink>
-        </h1>
-        <nav class="nav-menu d-none d-lg-block">
-          <ul>
-            <li class="active">
-              <NavLink to="/schools" className="mr-5">
-                Register
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/schools">Login</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
+    <div class="container d-flex align-items-center">
+      <h1 class="logo me-auto">
+        <NavLink to="/" exact className="link">
+          <GiBookmark className="logo" /> <span id="logo-name">e</span>
+          <span id="logo-text">Learning</span>
+        </NavLink>
+      </h1>
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active">
+            <NavLink to="/schools" className="mr-5">
+              Register
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/schools">Login</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
