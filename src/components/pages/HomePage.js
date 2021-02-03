64aa0a28281eaae6b02ebc873f3b5951f14608e4
eneamtiu <42860002+eneamtiu@users.noreply.github.com>
2021-02-04 00:45:05 +0200

    Add hero section

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -7,55 +7,73 @@ import SchoolsPage from "./SchoolsPage";
 
 const HomePage = () => {
   return (
-    <div className="d-flex flex-column">
-      <div
-        id="carouselExampleCaptions"
-        className="carousel slide"
-        data-ride="carousel"
+    <>
+      <section
+        id="hero"
+        class="d-flex justify-content-center align-items-center"
       >
-        <ol className="carousel-indicators">
-          <li
-            data-target="#carouselExampleCaptions"
-            data-slide-to="0"
-            className="active"
-          ></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
-        </ol>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={slide1} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h2>Online Schools System</h2>
-              <p>Finis coronat opus</p>
+        <div
+          class="container position-relative"
+          data-aos="zoom-in"
+          data-aos-delay="100"
+        >
+          <h1>Online Schools System</h1>
+          <h2>
+            Finis coronat opus, <br />
+            barba non facit philosophum
+          </h2>
+        </div>
+      </section>
+      <div className="d-flex flex-column">
+        <div
+          id="carouselExampleCaptions"
+          className="carousel slide"
+          data-ride="carousel"
+        >
+          <ol className="carousel-indicators">
+            <li
+              data-target="#carouselExampleCaptions"
+              data-slide-to="0"
+              className="active"
+            ></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+          </ol>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={slide1} className="d-block w-100" alt="..." />
+              <div className="carousel-caption d-none d-md-block">
+                <h2>Online Schools System</h2>
+                <p>Finis coronat opus</p>
+              </div>
             </div>
-          </div>
-          <div className="carousel-item">
-            <img src={slide2} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h2>Online Schools System</h2>
-              <p>Barba non facit philosophum</p>
+            <div className="carousel-item">
+              <img src={slide2} className="d-block w-100" alt="..." />
+              <div className="carousel-caption d-none d-md-block">
+                <h2>Online Schools System</h2>
+                <p>Barba non facit philosophum</p>
+              </div>
             </div>
-          </div>
-          <div className="carousel-item">
-            <img src={slide3} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h2>Online Schools System</h2>
-              <p>Vivere memento</p>
+            <div className="carousel-item">
+              <img src={slide3} className="d-block w-100" alt="..." />
+              <div className="carousel-caption d-none d-md-block">
+                <h2>Online Schools System</h2>
+                <p>Vivere memento</p>
+              </div>
             </div>
-          </div>
-          <div className="carousel-item">
-            <img src={slide4} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h2>Online Schools System</h2>
-              <p>Nemo dat quod non habet</p>
+            <div className="carousel-item">
+              <img src={slide4} className="d-block w-100" alt="..." />
+              <div className="carousel-caption d-none d-md-block">
+                <h2>Online Schools System</h2>
+                <p>Nemo dat quod non habet</p>
+              </div>
             </div>
           </div>
         </div>
+        <SchoolsPage />
       </div>
-      <SchoolsPage />
-    </div>
+    </>
   );
 };
 
