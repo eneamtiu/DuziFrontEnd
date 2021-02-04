e7672172a93dfc1f87c1e32151b74d3d8a77e4dd
eneamtiu <42860002+eneamtiu@users.noreply.github.com>
2021-02-04 04:30:18 +0200

    Add under-footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -105,6 +105,35 @@ const Footer = () => {
             </div>
           </div>
         </div>
+
+        <div className="container d-md-flex py-4">
+          <div className="me-md-auto text-center text-md-start">
+            <div className="copyright">
+              &copy; 2021 - Programmed by Coni &amp; Tudor
+            </div>
+            <div className="credits">
+              Graphical template provided by{" "}
+              <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+          </div>
+          <div className="social-links text-center text-md-right pt-3 pt-md-0">
+            <a href="/" className="twitter">
+              <i className="bx bxl-twitter"></i>
+            </a>
+            <a href="/" className="facebook">
+              <i className="bx bxl-facebook"></i>
+            </a>
+            <a href="/" className="instagram">
+              <i className="bx bxl-instagram"></i>
+            </a>
+            <a href="/" className="google-plus">
+              <i className="bx bxl-skype"></i>
+            </a>
+            <a href="/" className="linkedin">
+              <i className="bx bxl-linkedin"></i>
+            </a>
+          </div>
+        </div>
       </footer>
 
       <a href="/" className="back-to-top">
